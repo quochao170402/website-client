00c03196a0e4c0be7fdaba8751a054d6ac063778
quochao170402 <quochao170402@gmail.com>
2024-02-29 05:28:57 +0700

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,12 @@ import logo from './logo.svg';
 function App() {
   return (
     <div className="App">
+      <h1>Check CICD</h1>
       <Getter/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload Demo CICD.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
