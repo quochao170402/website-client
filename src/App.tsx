64ef7ec4b1eedd258b5001d4ce30eb103d3f6d53
quochao <quochao170402@gmail.com>
2024-02-29 10:21:03 +0700

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import './App.css';
-import Getter from './components/Getter';
-import logo from './logo.svg';
+import "./App.css";
+import Getter from "./components/Getter";
+import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="App">
       <h1>Check CICD</h1>
-      <Getter/>
+      <Getter />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Hello world
-        </h1>
+        <h1>Hello world</h1>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=ahRxhAzyMWI"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Click vào đây để xem Táo quân 2016
         </a>
       </header>
     </div>
